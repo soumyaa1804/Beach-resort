a4c8c0a0d8271df7355eee31ac8ac94648f615c3
Soumya Sharma <soum.sharma169@gmail.com>
2020-03-16 17:18:34 +0530

    initial commit

---
+++ src/components/RoomFilter.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import {useContext} from 'react';
+import {RoomContext} from '../context';
+import Title from '../components/Title';
+
+// get all unique values
+const getUnique = (items, value) => {
+    return [...new Set(items.map(item => item[value]))];
+};
+
+export default function RoomFilter({rooms}) {
+    const context = useContext(RoomContext);
+    const {
+        handleChange,
+        type,
+        capacity,
+        price,
+        minPrice,
+        maxPrice,
+        minSize,
+        maxSize,
+        breakfast,
+        pets
+    } = context;
+
+// get unique types
+let types = getUnique(rooms, 'type');
+// add all
+types = ['all', ...types];
+// map to jsx
+types = types.map((item, index) => {
+    return <option value={item} key={index}>{item}</option>
+});
+
+let people = getUnique(rooms, 'capacity');
+people = people.map((item, index) => {
+    return <option value={item} key={index}>{item}</option>
+})
+
+    return (
+        <section className="filter-container">
+            <Title title="search Rooms" />
+            <form className="filter-form">
+                {/* select type */}
+                    <div className="form-group">
+                        <label htmlFor="type">room type</label>
+                        <select 
+                            name="type"
+                            id="type"
+                            value={type}
+                            className="form-control"
+                            onChange={handleChange}
+                        >
+                            {types}
+                        </select>
+                    </div>
+                {/*end select type */}
+                {/* guest */}
+                <div className="form-group">
+                        <label htmlFor="capacity">Guests</label>
+                        <select 
+                            name="capacity"
+                            id="capacity"
+                            value={capacity}
+                            className="form-control"
+                            onChange={handleChange}
+                        >
+                            {people}
+                        </select>
+                    </div>
+                {/*end select type */}
+                {/* room price */}
+                <div className="form-group">
+                    <label htmlFor="price">
+                        room price ${price}
+                    </label>
+                    <input type="range" name="price" min={minPrice} max={maxPrice} id="price" value={price} onChange={handleChange} className="form-control" />
+                </div>
+                {/* end of room price */}
+            </form>
+        </section>
+    )
+}
---
+++ src/pages/Rooms.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+import RoomContainer from "../components/RoomContainer";
+
+const Rooms = () => {
+    return (
+        <>
+            <Hero hero="roomsHero">
+                <Banner title="rooms">
+                    <Link to="/" className="btn-primary">
+                        return home
+                    </Link>
+                </Banner>
+            </Hero>
+            <RoomContainer />
+        </>
+    );
+};
+
+export default Rooms;
+
+
